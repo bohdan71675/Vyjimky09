89739ac1e765cbb744c4172fa33ffa709f3832bd
bodnarb.04 <bodnarb.04@gmail.com>
2023-01-19 11:18:10 +0100

    hotovo

--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -22,27 +23,37 @@ namespace Vyjimky09
         {
             int pocet = 0;
             double soucet = 0;
+            int n = int.Parse(textBox1.Text);
             try
             {
                 using (FileStream fs = new FileStream("RealnaCisla.dat", FileMode.Open, FileAccess.Read))
                 {
                     BinaryReader br = new BinaryReader(fs);
-                    while (fs.Position < fs.Length)
+                    for(int i = 0; i < n; i++)
                     {
                         double cislo = br.ReadDouble();
                         soucet += cislo;
                         pocet++;
                     }
                 }
+                if (pocet > 0)
+                {
+                    double AP = soucet / pocet;
+                    MessageBox.Show("Prumer je " + AP);
+                }
+                else 
+                {
+                    MessageBox.Show("Nelze delit nulou");
+                }
             }
             catch (FileNotFoundException)
             {
                 MessageBox.Show("soubor nebyl nalezen, zkontroluj umístění a název soboru");
             }
-            catch (IOException)
-            {
-                MessageBox.Show("Data jsou poskozena");
-            }
+           // catch ()
+           // {
+          //      MessageBox.Show("Neni pristup k souboru");
+           // }
             catch (OverflowException)
             {
                 MessageBox.Show("Součet je příliš velké nebo malé číslo");
